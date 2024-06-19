8c9bb0813157c654727a715f0b4d8f137faa29c6
Thomaz Bazani <86675694+ThBanza@users.noreply.github.com>
2024-06-19 00:23:40 -0300

    atualizado

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import Conversor from './components/Conversor.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+    <Conversor class="conversor" moeda-a="USD" moeda-b="BRL" />
+  </main>
+</template>
+
+<style scoped>
+.conversor {
+  padding: 20px;
+  max-width: 300px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  position: center;
+  
+}
+</style>
---
+++ src/components/Conversor.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input type="text" v-model="realValue">
+    <button @click="convertToDollar">Convert</button>
+    <h2> BRL:{{ realValue }} para USD:{{ dollarValue }} </h2>
+    <h2>{{ dollarValue_value }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      realValue: 0,
+      dollarValue: 0,
+      exchangeRate: 0.20 // 1 real = 0.20 dollars (example rate)
+    }
+  },
+  methods: {
+    convertToDollar() {
+      this.dollarValue = this.realValue * this.exchangeRate;
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
